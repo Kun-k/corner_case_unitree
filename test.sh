f21175b3d55590c87958802e14aec848451740a6
renkun <renkun19@foxmail.com>
2026-03-02 10:10:56 +0800

    更新sh文件和txt记录文件

--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-#python legged_gym/scripts/play.py --task=go2 --experiment_name go2_exp --load_run Feb06_16-40-17_go2_exp_1 --num_envs 16 --seed 42 --max_iterations 10 --sim_device cuda:0 --rl_device cuda:1
-#python legged_gym/scripts/play.py --task=g1 --experiment_name g1_exp --run_name g1_exp_2 --num_envs 16 --seed 42 --max_iterations 10 --sim_device cuda:0 --rl_device cuda:1
-python train_terrain/test.py --task=go2_stair --experiment_name go2_stair_exp --num_envs 1 --seed 42 --max_iterations 10 --sim_device cuda:0 --rl_device cuda:1
+python train_terrain/train_terrain.py --task=go2_stair --experiment_name go2_terrain_2_target_exp --load_run Feb26_20-18-45_go2_terrain_2_target_exp_0 --num_envs 1 --seed 42 --max_iterations 100 --sim_device cuda:3 --rl_device cuda:3
--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
-#python legged_gym/scripts/train.py --task=go2 --experiment_name go2_exp --run_name go2_exp_2 --num_envs 4096 --seed 42 --max_iterations 10000 --sim_device cuda:2 --rl_device cuda:1 --headless
-#python legged_gym/scripts/train.py --task=g1 --experiment_name g1_exp --run_name g1_exp_2 --num_envs 4096 --seed 42 --max_iterations 10000 --sim_device cuda:2 --rl_device cuda:1 --headless
-python legged_gym/scripts/train.py --task=go2_stair --experiment_name go2_terrain_4_target_exp --run_name go2_terrain_4_target_exp_0 --num_envs 4096 --seed 42 --max_iterations 10000 --sim_device cuda:2 --rl_device cuda:1 --headless
+#python legged_gym/scripts/train.py --task=go2_stair --experiment_name go2_terrain_0_target_exp --run_name go2_terrain_0_target_exp_0 --num_envs 4096 --seed 42 --max_iterations 10000 --sim_device cuda:2 --rl_device cuda:1 --headless
+
+#nohup python legged_gym/scripts/train.py --task=go2_stair --experiment_name go2_terrain_0_exp --run_name go2_terrain_0_exp_0 --sim_device cuda:0 --rl_device cuda:0 --num_envs 4096 --seed 42 --max_iterations 10000 --headless >go2_terrain_0_exp_0.out 2>&1 &
+#nohup python legged_gym/scripts/train.py --task=go2_stair --experiment_name go2_terrain_2_exp --run_name go2_terrain_2_exp_0 --sim_device cuda:1 --rl_device cuda:1 --num_envs 4096 --seed 42 --max_iterations 10000 --headless >go2_terrain_2_exp_0.out 2>&1 &
+#nohup python legged_gym/scripts/train.py --task=go2_stair --experiment_name go2_terrain_4_exp --run_name go2_terrain_4_exp_0 --sim_device cuda:2 --rl_device cuda:2 --num_envs 4096 --seed 42 --max_iterations 10000 --headless >go2_terrain_4_exp_0.out 2>&1 &
